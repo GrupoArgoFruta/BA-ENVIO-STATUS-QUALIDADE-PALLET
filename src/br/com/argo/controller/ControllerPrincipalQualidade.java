b734d987f87a6790e12653b9d135d9c27f6d3858
Natanael lopes <natan1918@gmail.com>
2025-08-27 08:24:09 -0300

    novas classes

--- src/br/com/argo/controller/ControllerPrincipalQualidade.java
+++ src/br/com/argo/controller/ControllerPrincipalQualidade.java
@@ -1,5 +1,103 @@
 package br.com.argo.controller;
 
-public class ControllerPrincipalQualidade {
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import br.com.argo.service.ServiceCorpoEmailQualidade;
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.core.JapeSession;
+import br.com.sankhya.jape.core.JapeSession.SessionHandle;
+import br.com.sankhya.jape.dao.JdbcWrapper;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+
+public class ControllerPrincipalQualidade implements AcaoRotinaJava {
+
+	@Override
+	public void doAction(ContextoAcao ctx) throws Exception {
+		// TODO Auto-generated method stub
+		Registro[] linhas = ctx.getLinhas();
+	    JdbcWrapper jdbc = JapeFactory.getEntityFacade().getJdbcWrapper();
+	    SessionHandle hnd = JapeSession.open(); // Sempre feche depois
+	    ServiceCorpoEmailQualidade ServiceEmail = new ServiceCorpoEmailQualidade();
+	    try {
+	    	// Construir a tabela HTML com os dados das notas
+	        StringBuilder tabelaHtml = new StringBuilder();
+	        tabelaHtml.append("<table>")
+            .append("<tr>")
+            .append("<th>Nro. Único Pallet</th>")
+            .append("<th>Nro. Pallet</th>")
+            .append("<th>Local</th>")
+            .append("<th>Status Qualidade</th>")
+            .append("<th>Status </th>")
+            .append("<th>Observação Qualidade Pallet </th>")
+            .append("<th>Calibre </th>")
+            .append("<th>Qtd. Caixas Pallet </th>")
+            .append("</tr>");
+	    	for (Registro registro : linhas) {
+	    		
+	    		BigDecimal nUnico = (BigDecimal) registro.getCampo("NROUNICO");
+	    		String palett = (String) registro.getCampo("NROPALLET");
+	    		BigDecimal codlocal = (BigDecimal) registro.getCampo("CODLOCAL");
+	    		BigDecimal StatusQualidade = (BigDecimal) registro.getCampo("CODSTATUS");
+	    		String statusSigla = (String) registro.getCampo("STATUS");
+	    		String obsqualidade = (String) registro.getCampo("OBSQUALIDADE");
+	    		String calibre = (String) registro.getCampo("CALIBRE");
+	    		BigDecimal QtdCaixaPallet = (BigDecimal) registro.getCampo("QTDCAIXASPLT");
+//	    		OBSQUALIDADE CALIBRE QTDCAIXASPLT
+	    		String Status;
+
+	    		switch (statusSigla) {
+	    		    case "A":
+	    		        Status = "Aberto";
+	    		        break;
+	    		    case "F":
+	    		        Status = "Finalizado";
+	    		        break;
+	    		    case "D":
+	    		        Status = "Desmontado";
+	    		        break;
+	    		    case "R":
+	    		        Status = "Reprocessado";
+	    		        break;
+	    		    default:
+	    		        Status = "Desconhecido";
+	    		}
+	    		//buscar o local 
+	    		JapeWrapper locDAO = JapeFactory.dao("LocalFinanceiro");
+				DynamicVO localVO = locDAO.findByPK(codlocal);
+				String nomeLocal = localVO.asString("DESCRLOCAL");
+				
+				//buscar o Cadastro Status Pallet
+	    		JapeWrapper statusDAO = JapeFactory.dao("AD_STATUSPLT");
+				DynamicVO statusVO = statusDAO.findByPK(StatusQualidade);
+				String nomeStatus = statusVO.asString("DESCRICAO");
+				
+	    		tabelaHtml.append("<tr>")
+                .append("<td>").append(nUnico).append("</td>")
+                .append("<td>").append(palett).append("</td>")
+                .append("<td>").append(nomeLocal).append("</td>")
+                .append("<td>").append(nomeStatus).append("</td>")
+                .append("<td>").append(Status).append("</td>")
+                .append("<td>").append(obsqualidade).append("</td>")
+                .append("<td>").append(calibre).append("</td>")
+                .append("<td>").append(QtdCaixaPallet).append("</td>")
+                .append("</tr>");
+	    		
+	    	}
+	    	 tabelaHtml.append("</table>");
+	    	 Timestamp dthoraentrada = (Timestamp) ctx.getParam("DTENTRADA");
+			 Timestamp dthorasaida= (Timestamp) ctx.getParam("DTSAIDA");
+	    	 ServiceEmail.CorpoEmailStatusQualidade(ctx, tabelaHtml.toString(),dthoraentrada,dthorasaida);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			   ctx.mostraErro("Erro ao enviar o e-mail na classe principal: " + e.getMessage());
+		        e.printStackTrace();
+		}
+	}
 
 }
---
+++ src/br/com/argo/service/ServiceCorpoEmailQualidade.java
@@ -0,0 +1,136 @@
+package br.com.argo.service;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.EntityFacade;
+import br.com.sankhya.jape.core.JapeSession;
+import br.com.sankhya.jape.core.JapeSession.SessionHandle;
+import br.com.sankhya.jape.dao.JdbcWrapper;
+import br.com.sankhya.jape.sql.NativeSql;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.auth.AuthenticationInfo;
+import br.com.sankhya.modelcore.util.EntityFacadeFactory;
+import br.com.sankhya.ws.ServiceContext;
+
+public class ServiceCorpoEmailQualidade {
+	ServiceEmailStatuaQualidade email = new  ServiceEmailStatuaQualidade();
+	public void CorpoEmailStatusQualidade(ContextoAcao ctx, String tabelaHtml, Timestamp dthoraentrada, Timestamp dthorasaida) throws Exception {
+		JdbcWrapper jdbc = JapeFactory.getEntityFacade().getJdbcWrapper();
+		SessionHandle hnd = JapeSession.open();
+		NativeSql nativeSql = new NativeSql(jdbc);
+		EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+		String usuarioLogadoNome = ((AuthenticationInfo) ServiceContext.getCurrent().getAutentication()).getUsuVO().getNOMEUSU();
+		BigDecimal usuarioLogadoID = ((AuthenticationInfo) ServiceContext.getCurrent().getAutentication()).getUserID();
+		Timestamp dataAtual = new Timestamp(new Date().getTime());
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		String dataHoraAtualFormatada = sdf.format(dataAtual);
+		
+		String assunto = "Envio Status Qualidade";
+		// Formatar a data do parâmetro
+	    String dtentrada = dthoraentrada != null ? sdf.format(dthoraentrada) : "Data não informada";
+	    String dtsaida = dthorasaida != null ? sdf.format(dthorasaida) : "Data não informada";
+	 
+		// Construindo o valor a ser inserido no campo AD_PROTOCOLO
+        String infProtocolo = "Usuário: " + usuarioLogadoNome + "   |  ID: " + usuarioLogadoID + "   |   Data: " + dataHoraAtualFormatada;
+        // Pega o valor do parâmetro digitado (como String)
+       
+        try {
+        	Integer qtdpastilhas    = (Integer) ctx.getParam("QTDPASTILHA");
+        	Integer qtdativadores   = (Integer) ctx.getParam("QTDATIVADORES");
+        	Integer qtdpallet       = (Integer) ctx.getParam("QTDPALLET");
+
+        	
+           
+            String mensagem = "<!DOCTYPE html>" +
+                    "<html>" +
+                    "<head>" +
+                    "    <meta charset='utf-8'/>" +
+                    "    <title>Email</title>" +
+                    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">" +
+                    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css\">" +
+                    "    <style>" +
+                    "@import url(https://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100);" +
+                    "        body { font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; }" +
+                    "        .success-box {" +
+                    "            background-color: #e6ffed;" +
+                    "            border-left: 5px solid #28a745;" +
+                    "            padding: 15px;" +
+                    "            margin-bottom: 20px;" +
+                    "            border-radius: 5px;" +
+                    "            color: #155724;" +
+                    "            display: flex;" +
+                    "            align-items: center;" +
+                    "            font-size: 14px;" +
+                    "        }" +
+                    "        .success-box img {" +
+                    "            width: 20px;" +
+                    "            height: 20px;" +
+                    "            margin-right: 10px;" +
+                    "        }" +
+                    "        table {" +
+                    "            border-collapse: collapse;" +
+                    "            width: 100%;" +
+                    "            background-color: #fff;" +
+                    "            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);" +
+                    "        }" +
+                    "        th, td {" +
+                    "            border: 1px solid #ddd;" +
+                    "            padding: 8px;" +
+                    "            text-align: center;" +
+                    "        }" +
+                    "        th {" +
+                    "            background-color: #343a40;" +
+                    "            color: white;" +
+                    "        }" +
+                    "        tr:nth-child(even) { background-color: #f2f2f2; }" +
+                    "    </style>" +
+                    "</head>" +
+                    "<body>" +
+
+                    "    <table cellpadding='0' cellspacing='0' border='0'>" +
+                    "        <tr>" +
+                    "            <td align='center' style='background-color:#1e6533;'>" +
+                    "                <div style='padding: 10px;'>" +
+                    "                    <img border='0' style='width:17%;'" +
+                    "                         src='https://argofruta.com/wp-content/uploads/2021/05/Logo-text-green.png' alt='Logo'>" +
+                    "                </div>" +
+                    "            </td>" +
+                    "        </tr>" +
+                    "    </table>" +
+
+                    "    <div class='success-box'>" +
+                    "        <img src='https://img.icons8.com/color/48/checked--v1.png' alt='check' style='width:16px;height:16px;margin-right:6px;'>" +
+                    "<strong>" + infProtocolo + "       &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Data de Entrada : " + dtentrada +
+                    "   &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Data de Saida : " + dtsaida +
+
+                    "</strong><br>" +
+                    "    </div>" +
+					"<p style='margin-top: 4px; font-weight: bold;'>Quantidade de Pastilhas: " + qtdpastilhas + "</p>" +
+					"<p style='margin-top: 4px; font-weight: bold;'>Quantidade de Ativadores: " + qtdativadores + "</p>" +
+					"<p style='margin-top: 4px; font-weight: bold;'>Quantidade de Paletes: " + qtdpallet + "</p>" +
+                    
+
+                    tabelaHtml +
+
+                    "</body>" +
+                    "</html>";
+
+            email.enviarEmail(assunto, mensagem);
+        } catch (Exception e) {
+            e.printStackTrace(); // ou logue o erro
+        } finally {
+            JapeSession.close(hnd);
+            JdbcWrapper.closeSession(jdbc);
+            NativeSql.releaseResources(nativeSql);
+        }
+    }
+
+
+}
